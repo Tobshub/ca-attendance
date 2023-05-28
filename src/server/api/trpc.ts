90b4c4cc35c76e36f3d705026c1041aa6c6b3035
Tobshub <tobanigeorge@gmail.com>
2023-05-28 15:43:50 +0100

    include auth token in context

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -20,7 +20,7 @@ import { prisma } from "@/server/db";
  * These allow you to access things when processing a request, like the database, the session, etc.
  */
 
-type CreateContextOptions = Record<string, never>;
+type CreateContextOptions = { auth: string | undefined };
 
 /**
  * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
@@ -35,6 +35,7 @@ type CreateContextOptions = Record<string, never>;
 const createInnerTRPCContext = (_opts: CreateContextOptions) => {
   return {
     prisma,
+    ..._opts,
   };
 };
 
@@ -44,8 +45,9 @@ const createInnerTRPCContext = (_opts: CreateContextOptions) => {
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = (_opts: CreateNextContextOptions) => {
-  return createInnerTRPCContext({});
+export const createTRPCContext = ({ req }: CreateNextContextOptions) => {
+  const token = req.headers.authorization;
+  return createInnerTRPCContext({ auth: token });
 };
 
 /**
@@ -92,3 +94,9 @@ export const createTRPCRouter = t.router;
  * are logged in.
  */
 export const publicProcedure = t.procedure;
+
+const AuthMiddleware = t.middleware(({ ctx, next }) => {
+  return next({ ctx });
+});
+
+export const privateProcedure = t.procedure.use(AuthMiddleware);
