01f9e493af89c6823b2ffea2f1e6039df07ec8c9
Tobshub <tobanigeorge@gmail.com>
2023-05-28 15:44:10 +0100

    token util

--- src/server/utils/token.ts
+++ src/server/utils/token.ts
@@ -1,9 +1,31 @@
 import jwt from "jsonwebtoken";
+import { env } from "@/env.mjs";
+import { Err, Ok } from "./result";
+import { logger } from "./logger";
+
+const jwtSecret = env.JWT_SECRET;
 
 export const authToken = {
   generate: (payload: string) => {
     try {
-    } catch (error) {}
+      const token = jwt.sign(payload, jwtSecret);
+      return Ok(token);
+    } catch (error) {
+      logger.error(error, "Failed to generate token");
+      return Err({});
+    }
   },
-  verify: () => {},
+  verify: (token: string) => {
+    try {
+      const isValid = jwt.verify(token, jwtSecret) as string;
+      if (isValid) {
+        return Ok(isValid);
+      } else {
+        throw new Error("UNEXPECTED");
+      }
+    } catch (error) {
+      logger.error(error, "Failed to verify token");
+      return Err("Invalid Token");
+    }
+  },
 };
