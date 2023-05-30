196630d74f35b353a76a13facda3b34848e0c74a
Tobshub <tobanigeorge@gmail.com>
2023-05-30 13:46:35 +0100

    handle user token

---
+++ src/utils/client-token.ts
@@ -0,0 +1,31 @@
+const TOKEN = "rccg_ca_attendance_admin_token";
+
+let localStorage: Storage | undefined;
+
+if (typeof window !== "undefined") {
+  localStorage = window.localStorage;
+}
+
+export const ClientToken = {
+  set: (value: string) => {
+    if (!localStorage) return undefined;
+    // let token = JSON.stringify(value);
+    localStorage.setItem(TOKEN, value);
+    return value;
+  },
+  get: () => {
+    if (!localStorage) return undefined;
+    let token = localStorage.getItem(TOKEN);
+    return token ?? undefined;
+  },
+  remove: () => {
+    if (!localStorage) return undefined;
+    let token = localStorage.getItem(TOKEN);
+    if (token) {
+      localStorage.removeItem(TOKEN);
+      return token;
+    } else {
+      return undefined;
+    }
+  },
+};
