673a228be23777a0a718cc8321eb1b12b3be39d4
Tobshub <tobanigeorge@gmail.com>
2023-05-30 13:47:23 +0100

    redirect to login if token is missing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { ClientToken } from "@/utils/client-token";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const url = usePathname();
+  const router = useRouter();
+  useEffect(() => {
+    if (url !== "/auth") {
+      const token = ClientToken.get();
+      if (!token) {
+        router.push(`/login?cb=${url}`);
+      }
+    }
+  }, []);
   return <Component {...pageProps} />;
 };
 
