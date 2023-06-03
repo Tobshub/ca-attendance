1803e098d7bae3452dd3dbc1b4a89f813658cb92
Tobshub <tobanigeorge@gmail.com>
2023-06-03 11:33:05 +0100

    sign out button

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,32 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import Image from "next/image";
 import rccgLogo from "@/assets/rccg-logo.png";
+import { ClientToken } from "@/utils/client-token";
+import { useRouter } from "next/router";
 
 export const HeaderWithLogo = () => {
+  const router = useRouter();
+  const handleSignOut = () => {
+    ClientToken.remove();
+    router.push("/login").catch((_) => null);
+  };
   return (
     <AppBar>
       <Toolbar sx={{ gap: 2 }}>
         <Image src={rccgLogo} alt="RCCG Logo" width={32} height={32} />
-        <Typography variant="h6">Calvary Assembly Ikoyi</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <Typography variant="body1">Calvary Assembly Ikoyi</Typography>
+          <Button color="warning" variant="contained" onClick={handleSignOut}>
+            SIGN OUT
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
