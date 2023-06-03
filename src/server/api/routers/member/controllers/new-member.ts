f27dff2fb620dbe3f1cf46013653e365388dbfa2
Tobshub <tobanigeorge@gmail.com>
2023-06-03 11:18:52 +0100

    min phone number length

--- src/server/api/routers/member/controllers/new-member.ts
+++ src/server/api/routers/member/controllers/new-member.ts
@@ -1,13 +1,13 @@
 import { privateProcedure, tError } from "@/server/api/trpc";
 import { Ok } from "@/server/utils/result";
-import { PrismaClientValidationError } from "@prisma/client/runtime";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { z } from "zod";
 
 export const NewMember = privateProcedure
   .input(
     z.object({
       name: z.string(),
-      phoneNum: z.string(),
+      phoneNum: z.string().min(9),
       address: z.string().optional(),
       sex: z.enum(["MALE", "FEMALE"]),
     })
@@ -21,7 +21,7 @@ export const NewMember = privateProcedure
       return Ok(new_member);
     } catch (err) {
       logger.error(err, "Failed to add new Member");
-      if (err instanceof PrismaClientValidationError) {
+      if (err instanceof PrismaClientKnownRequestError) {
         throw new tError({
           code: "CONFLICT",
           message: "Member is already on record",
